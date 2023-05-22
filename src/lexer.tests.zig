2e1bcf35f0b99537899386370564db9dde46e2d7
Alex Ozerov <botkooper@gmail.com>
2023-05-22 12:06:36 +0000

    add test

--- src/lexer.tests.zig
+++ src/lexer.tests.zig
@@ -24,6 +24,63 @@ test "simple test" {
     }
 }
 
+test "simple test 2" {
+    const input =
+        \\let five = 5;
+        \\	let ten = 10;
+        \\	let add = fn(x, y) {
+        \\	x + y;
+        \\	};
+        \\	let result = add(five, ten);
+    ;
+    var lexer = getLexer(input);
+
+    const expectedTokens = [_]Token{
+        Token.LET,
+        Token{ .IDENT = "five" },
+        Token.ASSIGN,
+        Token{ .INT = "5" },
+        Token.SEMICOLON,
+        Token.LET,
+        Token{ .IDENT = "ten" },
+        Token.ASSIGN,
+        Token{ .INT = "10" },
+        Token.SEMICOLON,
+        Token.LET,
+        Token{ .IDENT = "add" },
+        Token.ASSIGN,
+        Token.FUNCTION,
+        Token.LPAREN,
+        Token{ .IDENT = "x" },
+        Token.COMMA,
+        Token{ .IDENT = "y" },
+        Token.RPAREN,
+        Token.LBRACE,
+        Token{ .IDENT = "x" },
+        Token.PLUS,
+        Token{ .IDENT = "y" },
+        Token.SEMICOLON,
+        Token.RBRACE,
+        Token.SEMICOLON,
+        Token.LET,
+        Token{ .IDENT = "result" },
+        Token.ASSIGN,
+        Token{ .IDENT = "add" },
+        Token.LPAREN,
+        Token{ .IDENT = "five" },
+        Token.COMMA,
+        Token{ .IDENT = "ten" },
+        Token.RPAREN,
+        Token.SEMICOLON,
+        Token.EOF,
+    };
+
+    for (expectedTokens) |expectedToken| {
+        const token = lexer.nextToken();
+        try std.testing.expectEqualDeep(token, expectedToken);
+    }
+}
+
 test "doubled" {
     const input = "10 == 10 != 9 && false || 5 <= 10 >= 5;";
     var lexer = getLexer(input);
